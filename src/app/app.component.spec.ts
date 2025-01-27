1a92beffa0b3caf6e63ecab3c731baf31c39ba00
Erwan Vallée <erwanp.vallee@gmail.com>
2025-01-27 19:22:33 +0100

    angular 19 et tailwind 4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-project' title`, () => {
+  it(`should have the 'tailwind-chapitre15-tailwind-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-project');
+    expect(app.title).toEqual('tailwind-chapitre15-tailwind-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tailwind-chapitre15-tailwind-angular');
   });
 });
